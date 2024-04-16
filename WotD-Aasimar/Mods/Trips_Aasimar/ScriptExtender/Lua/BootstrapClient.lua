9769763f2bdf45f32166d20e3d088e9b86603966
Trips <amelie.hoflack@gmail.com>
2024-04-16 22:43:29 +0200

    WotD:Aasimar Script Extender Version
    Initial Upload of WotD:Aasimar SE
    
    Now uses Script Extender by Norbyte

---
+++ WotD-Aasimar/Mods/Trips_Aasimar/ScriptExtender/Lua/BootstrapClient.lua
@@ -0,0 +1,9 @@
+
+    Ext.Vars.RegisterUserVariable("AAS_WingsChosen", {
+        Server = true,
+        Client = true,
+        WriteableOnClient = true,
+        SyncToClient = true,
+        SyncToServer = true
+    })
+    _P("WOTD: Registered User Variables")
